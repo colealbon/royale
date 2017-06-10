f1e6026111a92248b643645ed1a35ac76313bc87
Cole Albon <cole.albon@gmail.com>
2017-06-10 13:49:43 -0700

    add unit tests for savePGPPubkey

--- src/lib/savePGPPubkey.js
+++ src/lib/savePGPPubkey.js
@@ -2,6 +2,10 @@
 
 import {getFromStorage} from './getFromStorage';
 import {determineContentType} from './determineContentType';
+import notEmpty from '../../src/lib/notEmpty.js';
+import notCleartext from '../../src/lib/notCleartext.js';
+import notPGPPrivkey from '../../src/lib/notPGPPrivkey.js';
+import notPGPMessage from '../../src/lib/notPGPMessage.js';
 
 export function savePGPPubkey(PGPkeyArmor) {
     // save public key to storage only if it doesn't overwrite a private key
@@ -16,19 +20,32 @@ export function savePGPPubkey(PGPkeyArmor) {
             Promise.reject('Error: missing localStorage'):
             new Promise((resolve, reject) => {
                 let PGPkey = openpgp.key.readArmored(PGPkeyArmor);
-                getFromStorage(localStorage)(PGPkey.keys[0].users[0].userId.userid)
-                .then(existingKey => {
-                    return (!existingKey) ?
-                    Promise.resolve('none') :
-                    determineContentType(existingKey)(openpgp);
+                notEmpty(PGPkeyArmor)
+                .then(() => {
+                    return notCleartext(PGPkeyArmor)(openpgp)
                 })
-                .then(existingKeyType => {
-                    if (existingKeyType === 'PGPPrivkey'){
-                        resolve('pubkey ignored X- attempted overwrite privkey')
-                    } else {
-                        localStorage.setItem(PGPkey.keys[0].users[0].userId.userid, PGPkeyArmor)
-                        resolve(`public pgp key saved <- ${PGPkey.keys[0].users[0].userId.userid}`)
-                    }
+                .then(() => {
+                    return notPGPPrivkey(PGPkeyArmor)(openpgp)
+                })
+                .then(() => {
+                    return notPGPMessage(PGPkeyArmor)(openpgp)
+                    // fixme? throws Cannot read property \'users\' of undefined instead of "not PGPMessage content"
+                })
+                .then(() => {
+                    return getFromStorage(localStorage)(PGPkey.keys[0].users[0].userId.userid)
+                    .then(existingKey => {
+                        return (!existingKey) ?
+                        Promise.resolve('none') :
+                        determineContentType(existingKey)(openpgp);
+                    })
+                    .then(existingKeyType => {
+                        if (existingKeyType === 'PGPPrivkey'){
+                            resolve('pubkey ignored X- attempted overwrite privkey')
+                        } else {
+                            localStorage.setItem(PGPkey.keys[0].users[0].userId.userid, PGPkeyArmor)
+                            resolve(`public pgp key saved <- ${PGPkey.keys[0].users[0].userId.userid}`)
+                        }
+                    })
                 })
                 .catch((err) => reject(err))
             })
--- test/enabled.js
+++ test/enabled.js
@@ -1,10 +1,11 @@
+require('./lib/savePGPPubkey.js');
 require('./lib/broadcast.js');
-require('./lib/decryptPGPMessageWithKey')
-require('./lib/decryptPGPMessage')
-require('./lib/determineContentType.js')
+require('./lib/decryptPGPMessageWithKey');
+require('./lib/decryptPGPMessage');
+require('./lib/determineContentType.js');
 require('./lib/encryptClearText.js');
-require('./lib/getFromStorage.js')
-require('./lib/handlePost.js')
+require('./lib/getFromStorage.js');
+require('./lib/handlePost.js');
 // require('./lib/message.js');
 require('./lib/notCleartext.js');
 require('./lib/notEmpty.js');
@@ -14,5 +15,5 @@ require('./lib/notPGPPrivkey.js');
 require('./lib/notPGPPubkey.js');
 require('./lib/notUndefined.js');
 
-require('./app/index.js')
-require('./app/message.js')
+require('./app/index.js');
+require('./app/message.js');
